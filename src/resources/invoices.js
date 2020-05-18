472b79f3a210f78935b3558fbcdc54c2536614fa
Daniel Mejia <luisdanielmejia@outlook.com>
2020-05-18 13:41:28 -0500

    Invoices resource
    - Add the invoices resource to manipulate data from stripe.

---
+++ src/resources/invoices.js
@@ -0,0 +1,17 @@
+// Depedencies
+import BaseService from '../baseService';
+
+/**
+ * Invoices service.
+ * @author Daniel Mejia
+ * @class Invoices
+ */
+export class Invoices extends BaseService {
+  /**
+   * Resource url.
+   * @type { string }
+   */
+  resourceUrl = 'invoices';
+}
+
+export default Invoices;
