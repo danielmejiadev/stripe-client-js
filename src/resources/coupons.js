83f396515903f0000367abf1cb32bb9cde02f337
Daniel Mejia <luisdanielmejia@outlook.com>
2020-05-19 14:18:11 -0500

    Add resources
    - Add token resources.
    - Add coupons resources.

---
+++ src/resources/coupons.js
@@ -0,0 +1,17 @@
+// Depedencies
+import BaseService from '../baseService';
+
+/**
+ * Coupons service.
+ * @author Daniel Mejia
+ * @class Coupons
+ */
+export class Coupons extends BaseService {
+  /**
+   * Resource url.
+   * @type { string }
+   */
+  resourceUrl = 'coupons';
+}
+
+export default Coupons;
